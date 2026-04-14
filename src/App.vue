e1c58896553cd0729b7e6cc2bcd6570443cdc5d2
Samuel <samuelpapati@gmail.com>
2026-04-14 11:13:10 -0300

    Rotas implementadas

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,24 @@
 <script setup>
-  import NavBottom from '@/components/nav/NavBottom.vue'
-  import NavAside from '@/components/nav/NavAside.vue'
-  import NavTop from './components/nav/NavTop.vue';
-  import { useBreakpoints } from './composables/useBreakpoints';
-import BoxInfo from './components/BoxInfo.vue';
-import DashboardAdmin from './views/DashboardAdmin.vue';
-import Appointments from './views/Appointments.vue';
-import FinancesAdmin from './views/FinancesAdmin.vue';
-import TeamsAdmin from './views/TeamsAdmin.vue';
-import Profile from './views/Profile.vue';
-import NewAppointment from './views/newAppointments/NewAppointment.vue';
-import ProfessionalSelection from './views/newAppointments/ProfessionalSelection.vue';
-import DateSelection from './views/newAppointments/DateSelection.vue';
-import Confirmation from './views/newAppointments/Confirmation.vue';
-import Login from './views/Login.vue';
-  const { width } = useBreakpoints()
+import NavBottom from '@/components/nav/NavBottom.vue'
+import NavAside from '@/components/nav/NavAside.vue'
+import NavTop from './components/nav/NavTop.vue';
+
+import { useBreakpoints } from './composables/useBreakpoints';
+import { useRoute } from 'vue-router';
+
+const { width } = useBreakpoints()
+const route = useRoute()
 
 </script>
 
 <template>
-  <Login></Login>
-  <!-- <NavBottom v-if="width < 768"></NavBottom> -->
-  <!-- <NavAside v-if="width >= 768"></NavAside> -->
-  <!-- <NavTop v-if="width < 768"></NavTop> -->
-  <!-- <DashboardAdmin></DashboardAdmin> -->
-  <!-- <Appointments></Appointments> -->
-  <!-- <FinancesAdmin></FinancesAdmin> -->
-  <!-- <TeamsAdmin></TeamsAdmin>   -->
-  <!-- <Profile></Profile> -->
-  <!-- <NewAppointment></NewAppointment> -->
-  <!-- <ProfessionalSelection></ProfessionalSelection> -->
-  <!-- <DateSelection></DateSelection> -->
-  <!-- <Confirmation></Confirmation>   -->
+  <RouterView/>
+
+  <template v-if="route.path !== '/'">
+    <NavBottom v-if="width < 768"></NavBottom>
+    <NavAside v-if="width >= 768"></NavAside>
+    <NavTop v-if="width < 768"></NavTop>
+  </template>
 </template>
 
 <style lang="scss">
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,72 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Login from '@/views/Login.vue';
+import DashboardAdmin from '@/views/DashboardAdmin.vue';
+import Appointments from '@/views/Appointments.vue';
+import FinancesAdmin from '@/views/FinancesAdmin.vue';
+import TeamsAdmin from '@/views/TeamsAdmin.vue';
+import Profile from '@/views/Profile.vue';
+import NewAppointment from '@/views/newAppointments/NewAppointment.vue';
+import ProfessionalSelection from '@/views/newAppointments/ProfessionalSelection.vue';
+import DateSelection from '@/views/newAppointments/DateSelection.vue';
+import Confirmation from '@/views/newAppointments/Confirmation.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: DashboardAdmin
+  },
+  {
+    path: '/agendamentos',
+    name: 'agendamentos',
+    component: Appointments
+  },
+  {
+    path: '/financas',
+    name: 'financas',
+    component: FinancesAdmin
+  },
+  {
+    path: '/equipe',
+    name: 'equipe',
+    component: TeamsAdmin
+  },
+  {
+    path: '/equipe/julia.campos',
+    name: 'equipe/julia.campos',
+    component: Profile
+  },
+  {
+    path: '/novo-agendamento',
+    name: 'novo-agendamento',
+    component: NewAppointment
+  },
+  {
+    path: '/novo-agendamento/selecionar-profissional',
+    name: 'selecionar-profissional',
+    component: ProfessionalSelection
+  },
+  {
+    path: '/novo-agendamento/selecionar-data',
+    name: 'selecionar-data',
+    component: DateSelection
+  },
+  {
+    path: '/novo-agendamento/resumo',
+    name: 'resumo',
+    component: Confirmation
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: routes,
 })
 
 export default router
